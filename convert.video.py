645f7cc04d7cbe0db313f635ab9993e7299afedd
DevSamuelV <realalloutmusic@gmail.com>
2021-11-21 01:41:15 -0500

    Added the flag to change the frame output per second

--- convert.video.py
+++ convert.video.py
@@ -12,12 +12,14 @@ print("=============================================\n \n \n")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--video", help="path for video", dest='vid')
+parser.add_argument("--frame", help="frame per second",
+                    dest='frames', default="10")
 args = parser.parse_args()
 
 vid_path = args.vid
 
 if vid_path != None:
     os.system("ffmpeg -i " + vid_path +
-              " -vf fps=10 ./dataset/images/img%02d.jpg")
+              " -vf fps=" + str(args.frames) + " ./dataset/images/img%02d.jpg")
 else:
     print("::> sPlease define --video flag")
